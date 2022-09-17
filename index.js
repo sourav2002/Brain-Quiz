75124faa9d2c2979b3c706339a2414718cfa8bb5
sourav2002 <sainisourav233@gmail.com>
2022-09-17 16:39:23 +0530

    add all html attributes using javascript

--- index.js
+++ index.js
@@ -1,11 +1,110 @@
-const cards = document.querySelectorAll(".card");
-console.log(cards);
-cards.forEach((card) => card.addEventListener("click", flip));
+
 
 //VARIABLES
 var isFlipped = false;
 var firstCard;
 var secondCard;
+
+const topImage = {
+  imageSrc : './images/lco.png',
+  alt : 'lco',
+}
+
+const imageData = [
+  {
+    imageSrc :  "./images/evilface.png",
+    alt : 'evilface',
+  },
+  {
+    imageSrc :  "./images/angryface.png",
+    alt : "angryface",
+  },
+  {
+    imageSrc :  "./images/coolface.png",
+    alt : "coolface",
+  },
+  {
+    imageSrc : "./images/feverface.png",
+    alt : "feverface",
+  },
+  {
+    imageSrc :  "./images/vomitface.png",
+    alt : "vomitface",
+  },
+  {
+    imageSrc :  "./images/sneezeface.png",
+    alt : "sneezeface",
+  },
+  {
+    imageSrc :  "./images/mindblownface.png",
+    alt : "mindblownface",
+  },
+  {
+    imageSrc :  "./images/thinkingface.png",
+    alt : "thinkingface",
+  },
+
+]
+
+function title(){
+  const textdiv = document.createElement('div');
+  textdiv.classList.add("container");
+  const h1 = document.createElement('h1');
+  h1.classList.add("text-white", "text-center", "text-lg");
+  const text = document.createTextNode("How is your memory");
+  h1.appendChild(text);
+  textdiv.appendChild(h1);
+  return textdiv;
+}
+
+function generateBoard(){
+  const container = document.querySelector(".gameContainer");
+  const textdiv = title();
+  container.appendChild(textdiv);
+  imageData.forEach( (data) =>{    
+    for (let i =0; i<2; i++){
+      // div of both img
+      const div = document.createElement('div');
+      div.classList.add("card");
+      var att = document.createAttribute("data-image");
+      att.value = data.alt;
+      div.setAttributeNode(att);
+
+      //image 1
+      const img1 = getImage("front", data.imageSrc, data.alt);
+      //image 2
+      const img2 = getImage("back", topImage.imageSrc, topImage.alt);
+      div.append(img1, img2);
+      container.appendChild(div);
+    }
+  })
+  const btn = resetButton();
+  document.body.appendChild(btn);
+}
+
+// create image 
+function getImage(myclass, source, imageAlt){
+  const img = document.createElement('img');
+  img.classList.add(myclass);
+  img.src = source;
+  img.setAttribute('alt', imageAlt);
+  return img;
+}
+
+function resetButton(){
+  const btn = document.createElement("button");
+  btn.classList.add("bg-success", "btn-lg", "mx-auto", "mt-5", "d-block", "resetbtn", "btn-success", "btncss");
+  const text = document.createTextNode("Reset Board");
+  btn.appendChild(text);
+  return btn;
+}
+
+generateBoard();
+resetButton();
+shuffle();
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+cards.forEach((card) => card.addEventListener("click", flip));
 
 
 function flip ()  {
@@ -14,11 +113,11 @@ function flip ()  {
   if(!isFlipped){
     isFlipped = true;
     firstCard = this;
+    firstCard.removeEventListener('click', flip);
   }else{
     secondCard = this;
     checkIt();
   }
-
 }
 
 const checkIt = ()=>{
@@ -28,16 +127,15 @@ const checkIt = ()=>{
 
 const success = () =>{
   console.log("success...");
-  firstCard.removeEventListener('click', flip);
   secondCard.removeEventListener('click', flip);
   reset();
 }
 
 const fail = () =>{
   console.log("failed.....");
-  
   setTimeout(() => {
     firstCard.classList.remove("flip");
+    firstCard.addEventListener('click', flip);
     secondCard.classList.remove("flip");
     reset();
   }, 600);
@@ -47,7 +145,6 @@ const reset = () =>{
   isFlipped = false;
   firstCard = null;
   secondCard = null;
-
 }
 
 function shuffle(){
@@ -56,19 +153,16 @@ function shuffle(){
     card.style.order = index;
   })
 };
-window.addEventListener('load', shuffle);
 
-
-const resetbtn = document.querySelector(".btn");
+const resetbtn = document.querySelector(".resetbtn");
 resetbtn.addEventListener('click', ()=>{
   console.log("reset button clicked....");
   cards.forEach( (card) =>  {
-    if(card != null){
-      card.addEventListener('click', flip);
-      card.classList.remove("flip")
-    }
-
+    card.classList.remove("flip")
+    card.addEventListener('click', flip);
   });
   isFlipped = false;
-  shuffle();
+  setTimeout(() => {
+    shuffle();
+  }, 800);
 })
